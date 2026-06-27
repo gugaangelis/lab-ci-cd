66f80c4bb3ea8516a93867579c5e8a3e1d0a2a07
Gustavo Angelis <gugaangelis@gmail.com>
2026-06-27 14:39:24 -0300

    fix: some errors in app.yml

--- app/deploy/deploy.tf
+++ app/deploy/deploy.tf
@@ -32,6 +32,10 @@ resource "aws_ecs_service" "this" {
     container_port   = 8000
   }
 
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+
 }
 
 resource "aws_cloudwatch_log_group" "this" {
--- app/deploy/terraform.tfvars
+++ app/deploy/terraform.tfvars
@@ -1,5 +1,5 @@
 subnets_id = [
-  "sua-subnet-a",
-  "sua-subnet-b",
-  "sua-subnet-c"
+  "subnet-0092bf6d4805a6878",
+  "subnet-04e73c40ff10eee86",
+  "subnet-0a215e8e49d191dbf"
 ]
